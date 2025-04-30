c9639aec48450afc65c12206987b2ff1b50e6cf5
MuhammadAhsan7866 <solutionweb386@gmail.com>
2025-04-30 22:44:59 +0500

    new deisgn

--- src/components/app/LandingPage/Instructions.jsx
+++ src/components/app/LandingPage/Instructions.jsx
@@ -11,51 +11,47 @@ import {
   Link,
 } from "@chakra-ui/react";
 
-
 const YouTubeDownloadGuide = () => {
   return (
     <Box py={10} maxW="1240px" mx="auto">
       {/* Header */}
       <Text fontSize="40px" fontWeight="500" textAlign="center" mb={8}>
-      Download YouTube Videos: Your Complete Guide
+        Download YouTube Videos: Your Complete Guide
       </Text>
 
       {/* Step-by-step images */}
       <Flex justify="center" gap={10} flexWrap="wrap" mb={8}>
         <VStack spacing={4}>
-          
-          <Image src="/howto2.png" alt="Step 1"w={'90%'} />
-          <Text textAlign="center" maxW="360px" fontSize={'17px'}>
+          <Image src="/howto2.png" alt="Step 1" w="90%" />
+          <Text textAlign="center" maxW="360px" fontSize="17px">
             Get your favorite YouTube videos downloaded with these
             straightforward instructions:
           </Text>
         </VStack>
         <VStack spacing={4}>
-        
-          <Image src="/howto3.png" alt="Step 2" w={'90%'} />
-          <Text textAlign="center" maxW="360px" fontSize={'17px'}>
+          <Image src="/howto3.png" alt="Step 2" w="90%" />
+          <Text textAlign="center" maxW="360px" fontSize="17px">
             Find the video you want on YouTube and grab the URL from your
             browser.
           </Text>
         </VStack>
         <VStack spacing={4}>
-        
-          <Image src="/howto3.png" alt="Step 3"w={'90%'} />
-          <Text textAlign="center" maxW="360px" fontSize={'17px'}>
+          <Image src="/howto3.png" alt="Step 3" w="90%" />
+          <Text textAlign="center" maxW="360px" fontSize="17px">
             Insert the YouTube video link into our download box above.
           </Text>
         </VStack>
       </Flex>
 
       {/* Text Guide Section */}
-      <Box  mb={10}>
-        <Text fontSize="2xl" fontWeight="semibold" mb={2} textAlign={'center'}>
+      <Box mb={10}>
+        <Text fontSize="2xl" fontWeight="semibold" mb={2} textAlign="center">
           How to Download YouTube Videos Without an Add-on
         </Text>
         <Text color="gray.600" mb={4} textAlign="left">
           Learn how to download a YouTube video without a video downloader
-          add-on! Simply add 'ss' to the video URL to quickly save the video to
-          your device.
+          add-on! Simply add &apos;ss&apos; to the video URL to quickly save the
+          video to your device.
         </Text>
         <UnorderedList textAlign="left" maxW="1240px" mx="auto" spacing={2}>
           <ListItem>
@@ -69,7 +65,7 @@ const YouTubeDownloadGuide = () => {
             </Link>
           </ListItem>
           <ListItem>
-            Modified URL with 'ss':{" "}
+            Modified URL with &apos;ss&apos;:{" "}
             <Link
               color="blue.500"
               href="https://ssyoutube.com/watch?v=YOCmSsBfafg"
@@ -79,10 +75,7 @@ const YouTubeDownloadGuide = () => {
             </Link>
           </ListItem>
         </UnorderedList>
-      </Box >
-    
-   
-     
+      </Box>
     </Box>
   );
 };
